37ad2a1414667a05378aa55bae1414f844a2a1ef
YA <ubuntu@ip-10-147-198-39.ec2.internal>
2013-07-10 05:10:22 +0000

    Changed to read index.html

--- web.js
+++ web.js
@@ -1,10 +1,27 @@
+var fs = require ('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+var buffer = fs.readFileSync('./index.html');
+
+//var hello = buffer.toString('utf-8');
+//console.log(hello);
+
+
+
+
+response.send(buffer.toString('utf-8'));
+///  response.send('Hello World 2!');
+
+
 });
+
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

7b3281131d643e0f278384640b39c99ec2097193
YA <ubuntu@ip-10-164-43-155.ec2.internal>
2013-07-19 00:36:01 +0000

    applied css to index.html

--- web.js
+++ web.js
@@ -23,7 +23,7 @@ response.send(buffer.toString('utf-8'));
 
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
